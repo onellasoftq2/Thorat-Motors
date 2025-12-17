33200d96d05a8c2d77b1a414b78601c0f2ab7b95
Onella Soft <onellabatchq22025@gmail.com>
2025-12-17 06:14:06 +0000

    PS D:\Projects\Onella\Thorat\Thorat-Motors> npm run dev
    > nextn@0.1.0 d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
